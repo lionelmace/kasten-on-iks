79d4fff177a2f2672dced200381e9a727b9e0e81
Lionel Mace <lionel.mace@gmail.com>
2024-08-02 15:39:05 +0200

    fix endpoint again

--- terraform/storage-cos.tf
+++ terraform/storage-cos.tf
@@ -64,7 +64,7 @@ locals {
       cos_endpoint = nonsensitive(ibm_resource_key.cos-hmac-kasten.credentials["connection.postgres.authentication.username"])
       cos_access_key_id = nonsensitive(ibm_resource_key.cos-hmac-kasten.credentials["cos_hmac_keys.access_key_id"])
       cos_secret_access_key = nonsensitive(ibm_resource_key.cos-hmac-kasten.credentials["cos_hmac_keys.secret_access_key"])
-      cos_endpoint = ibm_resource_instance.cos.s3_endpoint_direct
+      cos_endpoint = ibm_cos_bucket.kasten-bucket.cos.s3_endpoint_direct
     }
   ]
 }
